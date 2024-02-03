0de232842ea42f3a48900b86f7f45f88093c68ad
alanmartinc <alancabot.ac@gmail.com>
2024-02-03 17:47:48 -0600

    feat: importaciones y exportaciones

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,1 +1,3 @@
-console.log('Hola Mundo desde app.js')
+const { emailTemplate } = require('./js-foundation/01-template');
+
+console.log(emailTemplate);
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,9 @@
+const emailTemplate = `
+<div>
+    <h1>Hi, {{name}}</h1>
+    <p>Thank you for you order.</p>
+</div>`;
+
+module.exports = {
+    emailTemplate
+}
